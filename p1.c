12c06efceea3f62b51238a7a8db7dc11523d425f
Kush_Godhaviya <godhaviya2611kush@gmail.com>
2025-11-18 22:25:57 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,39 @@
+// Simple C Program :
+
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello World");
+    
+    return 0;
+}
+
+/* On Windows (using MinGW/GCC):
+Similar process, but with different file extensions:
+
+PRACTICAL1.c - Source code
+
+PRACTICAL1.i - Preprocessed file
+
+PRACTICAL1.s - Assembly code
+
+PRACTICAL1.obj - Object file (instead of .o)
+
+PRACTICAL1.exe - Final executable */
+
+// ------------------------------------------------ //
+
+// On Linux (using GCC):
+
+/* Files created during compilation:
+
+display_name.c - Original source code file
+
+display_name.i - Preprocessed file (created with gcc -E)
+
+display_name.s - Assembly code (created with gcc -S)
+
+display_name.o - Object file (created with gcc -c)
+
+a.out or display_name - Final executable */
